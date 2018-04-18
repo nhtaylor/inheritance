82e8a74553cfebe3820afcff517352805770d12e
Nathan Taylor <Nathan@Mobile.local>
2018-04-18 14:35:50 -0400

    Fixed coding for more variables
    Education is now fixed (ftmp), comments formatting is more structured.
    
    Issues still present: inflation unaccounted for, self-employment measures, full-time measurement, issues with urban/rural residence, average parental age.

--- echo/e_clean.R
+++ echo/e_clean.R
@@ -1,10 +1,11 @@
 # 1. PROVIDING ACCURATE COLUMN NAMES
 colnames(categories)  <- c(
   ## 1979 & others
-  "id", "age_79", "ma_ed", "pa_ed",
-  "sibnum_79", "sample_id", "race", "sex",
+  "id", "age_79", "ma_ed", "pa_ed", "sibnum_79", 
+  "sample_id", "race", "sex",
   ## 1987
-  "pa_month87", "pa_year87", "pa_age87", "ma_month87", "ma_year87", "ma_age87",
+  "pa_month87", "pa_year87", "pa_age87", "ma_month87", "ma_year87", 
+  "ma_age87",
   ## 1988
   "pa_month88", "pa_year88", "pa_age88", "ma_month88", "ma_year88",
   "ma_age88", "degree_1988", "gift_1988", "value_1988", "networth_1988",
@@ -28,7 +29,7 @@ colnames(categories)  <- c(
   "kidnum_1993",
   ## 1994
   "degree_1994", "hours_1994", "employ_1994", "gift_1994", "value_1994", 
-  "networth_1994", "totinc_1994", "region_1994", "marstat_1994", "reside_1984",
+  "networth_1994", "totinc_1994", "region_1994", "marstat_1994", "reside_1994",
   "kidnum_1994",
   ## 1996
   "totinc_1996", "region_1996", "marstat_1996", "reside_1996", "kidnum_1996", 
@@ -57,9 +58,10 @@ colnames(categories)  <- c(
 )
 echo <- categories[, order(colnames(categories))]
 
+
 # 2. Cleaning one-use variables
 
-## sex to female
+## sex to FEMALE
 table(echo$sex)
 echo$sex <- echo$sex - 1
 table(echo$sex)
@@ -67,7 +69,8 @@ colnames(echo)[which(names(echo) == "sex")] <- "female"
 table(echo$female)
 echo <- echo[, order(colnames(echo))]
 
-## parental education
+## PARENTAL EDUCATION
+# mother education
 table(echo$ma_ed)
 echo$ma_ed <- ifelse(echo$ma_ed < 12, 0,
               ifelse(echo$ma_ed == 12, 1,
@@ -76,7 +79,7 @@ echo$ma_ed <- ifelse(echo$ma_ed < 12, 0,
               ifelse(echo$ma_ed > 17 & echo$ma_ed <= 21, 4,
                 NA)))))
 table(echo$ma_ed)
-## father education
+# father education
 table(echo$pa_ed)
 echo$pa_ed <- ifelse(echo$pa_ed < 12, 0,
               ifelse(echo$pa_ed == 12, 1,
@@ -90,15 +93,262 @@ echo$parent_ed <- ifelse(echo$ma_ed >= echo$pa_ed, echo$ma_ed,
                   ifelse(echo$pa_ed > echo$ma_ed, echo$pa_ed,
                   ifelse(is.na(echo$ma_ed) & is.na(echo$pa_ed), NA,
                     7)))
+table(echo$parent_ed)
 
+## AGE for all time periods
+echo$age_1988 <- echo$age_79 + 9
+echo$age_1989 <- echo$age_79 + 10
+echo$age_1990 <- echo$age_79 + 11
+echo$age_1992 <- echo$age_79 + 13
+echo$age_1993 <- echo$age_79 + 14
+echo$age_1994 <- echo$age_79 + 15
+echo$age_1996 <- echo$age_79 + 17
+echo$age_1998 <- echo$age_79 + 19
+echo$age_2000 <- echo$age_79 + 21
+echo$age_2004 <- echo$age_79 + 25
+echo$age_2008 <- echo$age_79 + 29
+echo$age_2012 <- echo$age_79 + 33
+ 
+## PARENTAL AGE 
+# father's age in '88
+sum(is.na(echo$pa_age87))
+echo$pa_age87 <- ifelse(!is.na(echo$pa_age87), echo$pa_age87,
+                 ifelse(is.na(echo$pa_age87), 87 - echo$pa_year87, 
+                        NA))
+sum(is.na(echo$pa_age87))
+echo$pa_age87 <- ifelse(!is.na(echo$pa_age87), echo$pa_age87,
+                 ifelse(is.na(echo$pa_age87), 87 - echo$pa_year88,
+                        NA))
+sum(is.na(echo$pa_age87))
+echo$pa_age87 <- ifelse(!is.na(echo$pa_age87), echo$pa_age87,
+                 ifelse(is.na(echo$pa_age87), echo$pa_age88 - 1, 
+                        NA))
+sum(is.na(echo$pa_age87))
+table(echo$pa_age87)
+echo$pa_age88 <- echo$pa_age87 + 1
+# mother's age in '88
+sum(is.na(echo$ma_age87))
+echo$ma_age87 <- ifelse(!is.na(echo$ma_age87), echo$ma_age87,
+                 ifelse(is.na(echo$ma_age87), 87 - echo$ma_year87, 
+                               NA))
+sum(is.na(echo$ma_age87))
+echo$ma_age87 <- ifelse(!is.na(echo$ma_age87), echo$ma_age87,
+                 ifelse(is.na(echo$ma_age87), 87 - echo$ma_year88,
+                               NA))
+sum(is.na(echo$ma_age87))
+echo$ma_age87 <- ifelse(!is.na(echo$ma_age87), echo$ma_age87,
+                 ifelse(is.na(echo$ma_age87), echo$ma_age88 - 1, 
+                               NA))
+sum(is.na(echo$ma_age87))
+table(echo$ma_age87)
+echo$ma_age88 <- echo$ma_age87 + 1
+## NEEDS PARENTAL AGE (avg > mom > parent > na) in '88
+echo$rental_age_1988 <- )
+
+## EDUCATION
+# swapping na's for zeros
+echo$degree_1988[is.na(echo$degree_1988)] <- as.numeric(0)
+echo$degree_1989[is.na(echo$degree_1989)] <- as.numeric(0)
+echo$degree_1990[is.na(echo$degree_1990)] <- as.numeric(0)
+echo$degree_1992[is.na(echo$degree_1992)] <- as.numeric(0)
+echo$degree_1993[is.na(echo$degree_1993)] <- as.numeric(0)
+echo$degree_1994[is.na(echo$degree_1994)] <- as.numeric(0)
+echo$degree_1996[is.na(echo$degree_1996)] <- as.numeric(0)
+echo$degree_1998[is.na(echo$degree_1998)] <- as.numeric(0)
+echo$degree_2000[is.na(echo$degree_2000)] <- as.numeric(0)
+echo$degree_2004[is.na(echo$degree_2004)] <- as.numeric(0)
+echo$degree_2008[is.na(echo$degree_2008)] <- as.numeric(0)
+echo$degree_2012[is.na(echo$degree_2012)] <- as.numeric(0)
+
+## EDUCATION
+# all years in same categories of education
+# 0 - less than hs diploma
+# 1 - hs diploma
+# 2 - some college
+# 3 - bachelor's degree
+# 4 - advanced degree
+## FIRST PART SHIFT EACH VARIABLE TO NEW SCALE
+## THEN CODE UPDATES TO TOTAL EDUCATION ATTAINMENT & NOT DIFFERENCES
+
+# 1988
+table(echo$degree_1988)
+echo$degree_1988 <- ifelse(echo$degree_1988 == 1, 1,
+                    ifelse(echo$degree_1988 == 2, 2,
+                    ifelse(echo$degree_1988 == 3 | echo$degree_1988 == 4, 3,
+                    ifelse(echo$degree_1988 >= 5, 4,
+                      0))))
+table(echo$degree_1988)
+
+# 1989
+table(echo$degree_1989)
+echo$degree_1989 <- ifelse(echo$degree_1989 == 1, 1,
+                    ifelse(echo$degree_1989 == 2, 2,
+                    ifelse(echo$degree_1989 == 3 | echo$degree_1989 == 4, 3,
+                    ifelse(echo$degree_1989 >= 5, 4,
+                      0))))
+table(echo$degree_1989)
+echo$degree_1989 <- ifelse(echo$degree_1989 > echo$degree_1988, echo$degree_1989,
+                           echo$degree_1988)
+table(echo$degree_1989)
+
+
+# 1990
+table(echo$degree_1990)
+echo$degree_1990 <- ifelse(echo$degree_1990 == 1, 1,
+                    ifelse(echo$degree_1990 == 2, 2,
+                    ifelse(echo$degree_1990 == 3 | echo$degree_1990 == 4, 3,
+                    ifelse(echo$degree_1990 >= 5, 4,
+                      0))))
+table(echo$degree_1990)
+echo$degree_1990 <- ifelse(echo$degree_1990 > echo$degree_1989, echo$degree_1990,
+                           echo$degree_1989)
+table(echo$degree_1990)
+
+# 1992
+table(echo$degree_1992)
+echo$degree_1992 <- ifelse(echo$degree_1992 == 1, 1,
+                    ifelse(echo$degree_1992 == 2, 2,
+                    ifelse(echo$degree_1992 == 3 | echo$degree_1992 == 4, 3,
+                    ifelse(echo$degree_1992 >= 5, 4,
+                       0))))
+table(echo$degree_1992)
+echo$degree_1992 <- ifelse(echo$degree_1992 > echo$degree_1990, echo$degree_1992,
+                           echo$degree_1990)
+table(echo$degree_1992)
+
+# 1993
+table(echo$degree_1993)
+echo$degree_1993 <- ifelse(echo$degree_1993 == 1, 1,
+                    ifelse(echo$degree_1993 == 2, 2,
+                    ifelse(echo$degree_1993 == 3 | echo$degree_1993 == 4, 3,
+                    ifelse(echo$degree_1993 >= 5, 4,
+                      0))))
+table(echo$degree_1993)
+echo$degree_1993 <- ifelse(echo$degree_1993 > echo$degree_1992, echo$degree_1993,
+                           echo$degree_1992)
+table(echo$degree_1993)
+
+# 1994
+table(echo$degree_1994)
+echo$degree_1994 <- ifelse(echo$degree_1994 == 1, 1,
+                    ifelse(echo$degree_1994 == 2, 2,
+                    ifelse(echo$degree_1994 == 3 | echo$degree_1994 == 4, 3,
+                    ifelse(echo$degree_1994 >= 5, 4,
+                      0))))
+table(echo$degree_1994)
+echo$degree_1994 <- ifelse(echo$degree_1994 > echo$degree_1993, echo$degree_1994,
+                           echo$degree_1993)
+table(echo$degree_1994)
+
+# 1996
+table(echo$degree_1996)
+echo$degree_1996 <- ifelse(echo$degree_1996 == 1, 1,
+                    ifelse(echo$degree_1996 == 2, 2,
+                    ifelse(echo$degree_1996 == 3 | echo$degree_1996 == 4, 3,
+                    ifelse(echo$degree_1996 >= 5, 4,
+                      0))))
+table(echo$degree_1996)
+echo$degree_1996 <- ifelse(echo$degree_1996 > echo$degree_1994, echo$degree_1996,
+                           echo$degree_1994)
+table(echo$degree_1996)
+
+# 1998
+table(echo$degree_1998)
+echo$degree_1998 <- ifelse(echo$degree_1998 == 1, 1,
+                    ifelse(echo$degree_1998 == 2, 2,
+                    ifelse(echo$degree_1998 == 3 | echo$degree_1998 == 4, 3,
+                    ifelse(echo$degree_1998 >= 5, 4,
+                      0))))
+table(echo$degree_1998)
+echo$degree_1998 <- ifelse(echo$degree_1998 > echo$degree_1996, echo$degree_1998,
+                           echo$degree_1996)
+table(echo$degree_1998)
+
+# 2000
+table(echo$degree_2000)
+echo$degree_2000 <- ifelse(echo$degree_2000 == 1, 1,
+                    ifelse(echo$degree_2000 == 2, 2,
+                    ifelse(echo$degree_2000 == 3 | echo$degree_2000 == 4, 3,
+                    ifelse(echo$degree_2000 >= 5, 4,
+                      0))))
+table(echo$degree_2000)
+echo$degree_2000 <- ifelse(echo$degree_2000 > echo$degree_1998, echo$degree_2000,
+                           echo$degree_1998)
+table(echo$degree_2000)
+
+# 2004
+table(echo$degree_2004)
+echo$degree_2004 <- ifelse(echo$degree_2004 == 1, 1,
+                    ifelse(echo$degree_2004 == 2, 2,
+                    ifelse(echo$degree_2004 == 3 | echo$degree_2004 == 4, 3,
+                    ifelse(echo$degree_2004 >= 5, 4,
+                      0))))
+table(echo$degree_2004)
+echo$degree_2004 <- ifelse(echo$degree_2004 > echo$degree_2000, echo$degree_2004,
+                           echo$degree_2000)
+table(echo$degree_2004)
+
+
+# 2008
+table(echo$degree_2008)
+echo$degree_2008 <- ifelse(echo$degree_2008 == 1, 1,
+                    ifelse(echo$degree_2008 == 2, 2,
+                    ifelse(echo$degree_2008 == 3 | echo$degree_2008 == 4, 3,
+                    ifelse(echo$degree_2008 >= 5, 4,
+                      0))))
+table(echo$degree_2008)
+echo$degree_2008 <- ifelse(echo$degree_2008 > echo$degree_2004, echo$degree_2008,
+                           echo$degree_2004)
+table(echo$degree_2008)
+
+# 2012
+table(echo$degree_2012)
+echo$degree_2012 <- ifelse(echo$degree_2012 == 1, 1,
+                    ifelse(echo$degree_2012 == 2, 2,
+                    ifelse(echo$degree_2012 == 3 | echo$degree_2012 == 4, 3,
+                    ifelse(echo$degree_2012 >= 5, 4,
+                      0))))
+table(echo$degree_2012)
+echo$degree_2012 <- ifelse(echo$degree_2012 > echo$degree_2008, echo$degree_2012,
+                           echo$degree_2008)
+table(echo$degree_2012)
+
+### NEEDS WORK
+## EMPLOY to self-employment
+echo$employ_1988 <- ifelse(echo$employ_1988 == 3, 1, 0)
+echo$employ_1989 <- 
+echo$employ_1990 <- 
+echo$employ_1992 <- 
+echo$employ_1993 <- 
+echo$employ_1994 <- 
+echo$employ_1996 <- 
+echo$employ_1998 <- 
+echo$employ_2000 <- 
+echo$employ_2004 <- 
+echo$employ_2008 <- 
+echo$employ_2012 <- )
+
+### NEED WORKS
+## FULL TIME EMPLOYMENT
+echo$hours_2012 <- )
+
+## RURAL-URBAN RESIDENCE
+
+
+## NUMBER OF KIDS
+# good to go!
+  
+## REGION
+# also good to go!
+  
 ## 3. Checking NA'S 
 nac <- sapply(delta, function(y) sum(length(which(is.na(y)))))
 nac <- data.frame(nac)
 nac
 
 ## removing na's based on 2012 net worth (whole sample present) -- no longer beneficial
-# d <- delta[!is.na(d$kidnum_2012), ]
-# dim(d)
+## d <- echo[!is.na(echo$kidnum_2012), ]
+## dim(d)
 
 ## checking na's again
 # na_fix <-sapply(d, function(y) sum(length(which(is.na(y)))))
